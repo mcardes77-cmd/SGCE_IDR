b2e8d06d5a234872e7a6486f2ed23f6af022236f
mcardes77-cmd <mcardes77@gmail.com>
2026-03-17 14:00:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -14,45 +15,9 @@ def health():
 def gestao_ocorrencia():
     return render_template("gestao_ocorrencia.html")
 
-@app.route("/gestao_ocorrencia_nova")
-def gestao_ocorrencia_nova():
-    return render_template("gestao_ocorrencia_nova.html")
-
-@app.route("/gestao_ocorrencia_editar")
-def gestao_ocorrencia_editar():
-    return render_template("gestao_ocorrencia_editar.html")
-
-@app.route("/gestao_ocorrencia_aberta")
-def gestao_ocorrencia_aberta():
-    return render_template("gestao_ocorrencia_aberta.html")
-
-@app.route("/gestao_ocorrencia_finalizada")
-def gestao_ocorrencia_finalizada():
-    return render_template("gestao_ocorrencia_finalizada.html")
-
-@app.route("/gestao_relatorio_impressao")
-def gestao_relatorio_impressao():
-    return render_template("gestao_relatorio_impressao.html")
-
 @app.route("/gestao_frequencia")
 def gestao_frequencia():
     return render_template("gestao_frequencia.html")
-
-@app.route("/gestao_frequencia_registro")
-def gestao_frequencia_registro():
-    return render_template("gestao_frequencia_registro.html")
-
-@app.route("/gestao_frequencia_atraso")
-def gestao_frequencia_atraso():
-    return render_template("gestao_frequencia_atraso.html")
-
-@app.route("/gestao_frequencia_saida")
-def gestao_frequencia_saida():
-    return render_template("gestao_frequencia_saida.html")
-
-@app.route("/gestao_relatorio_frequencia")
-def gestao_relatorio_frequencia():
-    return render_template("gestao_relatorio_frequencia.html")
 
 @app.route("/gestao_tecnologia")
 def gestao_tecnologia():
@@ -67,4 +32,5 @@ def gestao_cadastro():
     return render_template("gestao_cadastro.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
