acf1299e264a913672f9dd9f5031cdac21e5c390
mcardes77-cmd <mcardes77@gmail.com>
2026-03-17 10:52:55 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import render_template
+
+from flask import Flask, render_template
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/gestao_ocorrencia_nova")
+def nova():
+    return render_template("gestao_ocorrencia_nova.html")
+
+@app.route("/gestao_ocorrencia_editar")
+def editar():
+    return render_template("gestao_ocorrencia_editar.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
