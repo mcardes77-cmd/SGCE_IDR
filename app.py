391bdbce4318849d2350cc70435b4e2543f4752a
mcardes77-cmd <mcardes77@gmail.com>
2026-03-17 13:18:05 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,70 @@
-
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/health")
+def health():
+    return "OK"
+
+@app.route("/gestao_ocorrencia")
+def gestao_ocorrencia():
+    return render_template("gestao_ocorrencia.html")
+
 @app.route("/gestao_ocorrencia_nova")
-def nova():
+def gestao_ocorrencia_nova():
     return render_template("gestao_ocorrencia_nova.html")
 
 @app.route("/gestao_ocorrencia_editar")
-def editar():
+def gestao_ocorrencia_editar():
     return render_template("gestao_ocorrencia_editar.html")
+
+@app.route("/gestao_ocorrencia_aberta")
+def gestao_ocorrencia_aberta():
+    return render_template("gestao_ocorrencia_aberta.html")
+
+@app.route("/gestao_ocorrencia_finalizada")
+def gestao_ocorrencia_finalizada():
+    return render_template("gestao_ocorrencia_finalizada.html")
+
+@app.route("/gestao_relatorio_impressao")
+def gestao_relatorio_impressao():
+    return render_template("gestao_relatorio_impressao.html")
+
+@app.route("/gestao_frequencia")
+def gestao_frequencia():
+    return render_template("gestao_frequencia.html")
+
+@app.route("/gestao_frequencia_registro")
+def gestao_frequencia_registro():
+    return render_template("gestao_frequencia_registro.html")
+
+@app.route("/gestao_frequencia_atraso")
+def gestao_frequencia_atraso():
+    return render_template("gestao_frequencia_atraso.html")
+
+@app.route("/gestao_frequencia_saida")
+def gestao_frequencia_saida():
+    return render_template("gestao_frequencia_saida.html")
+
+@app.route("/gestao_relatorio_frequencia")
+def gestao_relatorio_frequencia():
+    return render_template("gestao_relatorio_frequencia.html")
+
+@app.route("/gestao_tecnologia")
+def gestao_tecnologia():
+    return render_template("gestao_tecnologia.html")
+
+@app.route("/gestao_tutoria")
+def gestao_tutoria():
+    return render_template("gestao_tutoria.html")
+
+@app.route("/gestao_cadastro")
+def gestao_cadastro():
+    return render_template("gestao_cadastro.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
